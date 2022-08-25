32c5e73a09c17681214e7fd0e2f4c0ad9716953a
sienna-sterling <112006749+sienna-sterling@users.noreply.github.com>
2022-08-24 22:03:26 -0400

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -9,7 +9,7 @@ public class Hello {
 	public static void main(String[] args) {
 		System.out.println("Sienna");
 		System.out.println("Hello.java");
-		System.out.println("The purpose of this program/project is to help expose us to coding and familiize us with the different programs and techniques we will have to untilize by learning how to code what we wrote above");
+		System.out.println("The purpose of this program/project is to help expose us to coding and familiize us with the different programs and 		techniques we will have to untilize by learning how to code what we wrote above");
 		System.out.println("08/24/22");
 		System.out.println("CMSC255 004");
 		System.out.println();
